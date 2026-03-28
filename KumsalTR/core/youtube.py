0387a97ff92e31d21aa4fac35eb0149a4a7d6849
turk65169 <turk65169@gmail.com>
2026-03-28 17:51:11 +0300

    Update youtube.py

--- KumsalTR/core/youtube.py
+++ KumsalTR/core/youtube.py
@@ -131,12 +131,11 @@ class YouTube:
                     "cookiefile": cookie,
                     "extractor_args": {
                         "youtube": {
-                            "player_client": ["android", "ios", "mweb"],
                             "skip": ["dash", "hls"],
                         }
                     },
                     "http_headers": {
-                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
                         "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
                     },
                     "format": fmt,
@@ -159,3 +158,4 @@ class YouTube:
                     continue
 
         return None
+
